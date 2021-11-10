c9114587cdd5e18aa89e75684ea475ea3073d4bb
PajiloyKoder <goryun1998@gmail.com>
2021-11-10 15:05:20 +0200

    Ну я тут чутка покакал
    Вчера особо не какалось, но что есть то есть.

--- src/google-docs-connection/google-docs-connection.controller.ts
+++ src/google-docs-connection/google-docs-connection.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { GoogleDocsConnectionService } from './google-docs-connection.service';
 
 @Controller('google-docs-connection')
-export class GoogleDocsConnectionController {}
+export class GoogleDocsConnectionController {
+    constructor(
+        private googleDocsConnectionService: GoogleDocsConnectionService, 
+        private httpService: HttpService,
+        ){};
+
+        // @Get()
+        // async Dosomesing (){
+        //  let zelupa =  this.googleDocsConnectionService.сlientAuth();
+        //   return zelupa;
+        // }
+        
+  
+
+}
+
+
+// @Get()
+//   @UseGuards(AuthGuard())
+//   async findCurrentUser(@User() {email}: UserEntity){
+//     const user = await this.authService.findCurrentUser(email)
+//     return user
+//   }
+
+
+
+
+
+
+
+
+
+
--- src/google-docs-connection/google-docs-connection.service.ts
+++ src/google-docs-connection/google-docs-connection.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import{google} from 'googleapis'
+import * as keys from './google-docs-keys.json';
 
+// const keys = require('./google-docs-keys.json')
+
+const client = new google.auth.JWT(
+    keys.client_email, null, keys.private_key, ['https://www.googleapis.com/auth/drive.file']
+    )
+
+client.authorize(function(err, tokens) {
+    if(err){
+        console.log(err);
+        return;
+    }
+    else{
+        console.log('Zaebis')
+        
+    }
+    
+})
 @Injectable()
-export class GoogleDocsConnectionService {}
+export class GoogleDocsConnectionService {
+    
+        
+   
+}
